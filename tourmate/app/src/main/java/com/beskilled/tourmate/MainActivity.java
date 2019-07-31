3258ec711db1440a6db010c10fe9788a48e8071e
mostafiz9900 <mostafiz9900@gmail.com>
2019-08-01 01:18:14 +0600

    home

--- tourmate/app/src/main/java/com/beskilled/tourmate/MainActivity.java
+++ tourmate/app/src/main/java/com/beskilled/tourmate/MainActivity.java
@@ -1,14 +1,34 @@
 package com.beskilled.tourmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.beskilled.tourmate.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+private ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        mainBinding.homeCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        mainBinding.tripsCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),TripsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ tourmate/app/src/main/java/com/beskilled/tourmate/TripsActivity.java
@@ -0,0 +1,48 @@
+package com.beskilled.tourmate;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TripsActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_trips:
+                    mTextMessage.setText(R.string.title_trips);
+                    return true;
+                case R.id.navigation_memorirics:
+                    mTextMessage.setText(R.string.title_memories);
+                    return true;
+                case R.id.navigation_wallet:
+                    mTextMessage.setText(R.string.title_wallet);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
