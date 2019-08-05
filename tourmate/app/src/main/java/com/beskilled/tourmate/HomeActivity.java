6942f006c6563fc6380609af112007cb9100c7d8
mostafiz9900 <mostafiz9900@gmail.com>
2019-08-05 11:04:56 +0600

    toolBar

--- tourmate/app/src/main/java/com/beskilled/tourmate/HomeActivity.java
+++ tourmate/app/src/main/java/com/beskilled/tourmate/HomeActivity.java
@@ -1,14 +1,28 @@
 package com.beskilled.tourmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.beskilled.tourmate.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_home);
+
+       binding.backBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               onBackPressed();
+           }
+       });
+
     }
 }
--- tourmate/app/src/main/java/com/beskilled/tourmate/MainActivity.java
+++ tourmate/app/src/main/java/com/beskilled/tourmate/MainActivity.java
@@ -5,16 +5,31 @@ import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.beskilled.tourmate.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 private ActivityMainBinding mainBinding;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
+        setSupportActionBar(mainBinding.toolBar);
+
+
+
 
         mainBinding.homeCV.setOnClickListener(new View.OnClickListener() {
             @Override
