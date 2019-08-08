fb5a5e49ca994fdd9917eea587f37a2e8aae8d8c
mostafiz9900 <mostafiz9900@gmail.com>
2019-08-08 23:32:03 +0600

    signin and sign up complite

--- tourmate/app/src/main/java/com/beskilled/tourmate/SignInActivity.java
+++ tourmate/app/src/main/java/com/beskilled/tourmate/SignInActivity.java
@@ -1,26 +1,94 @@
 package com.beskilled.tourmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
 import com.beskilled.tourmate.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
-private ActivitySignInBinding binding;
+    private ActivitySignInBinding binding;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_sign_in);
-        binding.loginToSignupBtn.setOnClickListener(new View.OnClickListener() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        init();
+        clickButton();
+
+    }
+
+    private void init() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void clickButton() {
+        binding.signInToUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
                 startActivity(intent);
             }
         });
+        binding.signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.emailSignInEt.getText().toString().trim();
+                String password = binding.passwordSignInEt.getText().toString().trim();
+               binding.signInPB.setVisibility(View.VISIBLE);
+                if (email.isEmpty()) {
+                    binding.emailSignInEt.setError("Enter Your Email Address");
+                    binding.emailSignInEt.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.emailSignInEt.setError("Does not Match email addresss");
+                    binding.emailSignInEt.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()) {
+                    binding.passwordSignInEt.setError("Enter Your Password");
+                    binding.passwordSignInEt.requestFocus();
+                    return;
+                }
+                if (password.length() < 6) {
+                    binding.passwordSignInEt.setError("Minimum Length of a password should be 6");
+                    binding.passwordSignInEt.requestFocus();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        binding.signInPB.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            finish();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SignInActivity.this, "not login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                // userLogin();
+            }
+        });
     }
+
+   /* private void userLogin() {
+
+
+    }*/
 }
--- tourmate/app/src/main/java/com/beskilled/tourmate/SignUpActivity.java
+++ tourmate/app/src/main/java/com/beskilled/tourmate/SignUpActivity.java
@@ -1,14 +1,111 @@
 package com.beskilled.tourmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
+import com.beskilled.tourmate.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
-
+private ActivitySignUpBinding binding;
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
+
+        init();
+        clickButton();
+
+    }
+
+    private void init() {
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void clickButton() {
+
+        binding.signUpToIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userRegestation();
+            }
+        });
+    }
+
+    private void userRegestation() {
+        final String userName=binding.nameSignUpEt.getText().toString().trim();
+        final String email=binding.emailSignUpEt.getText().toString().trim();
+        String password=binding.passwordSignUpEt.getText().toString().trim();
+        binding.signupPB.setVisibility(View.VISIBLE);
+        if (userName.isEmpty()) {
+            binding.nameSignUpEt.setError("Enter Your Name");
+            binding.nameSignUpEt.requestFocus();
+            return;
+        }if (email.isEmpty()) {
+            binding.emailSignUpEt.setError("Enter Your Email Address");
+            binding.emailSignUpEt.requestFocus();
+            return;
+        }if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.emailSignUpEt.setError("Does not Match email addresss");
+            binding.emailSignUpEt.requestFocus();
+            return;
+        }if (password.isEmpty()) {
+            binding.passwordSignUpEt.setError("Enter Your Password");
+            binding.passwordSignUpEt.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            binding.passwordSignUpEt.setError("Minimum Length of a password should be 6");
+            binding.passwordSignUpEt.requestFocus();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                binding.signupPB.setVisibility(View.GONE);
+           if (task.isSuccessful()){
+               String userId=mAuth.getCurrentUser().getUid();
+               DatabaseReference userRef=databaseReference.child("users").child(userId);
+               HashMap<String,Object> userMap=new HashMap<>();
+               userMap.put("userName",userName);
+               userMap.put("email",email);
+               userRef.setValue(userMap);
+               finish();
+           }
+           else {
+               if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                   Toast.makeText(getApplicationContext(), "user is alredy register", Toast.LENGTH_SHORT).show();
+               }else {
+                   Toast.makeText(getApplicationContext(), "Error "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+               }
+           }
+            }
+        });
     }
 }
