70eafc1a72c49115dd3767be21222e444a3c9373
mostafiz9900 <mostafiz9900@gmail.com>
2019-08-08 12:51:10 +0600

    signin  and singup des

---
+++ tourmate/app/src/main/java/com/beskilled/tourmate/SignInActivity.java
@@ -0,0 +1,26 @@
+package com.beskilled.tourmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.beskilled.tourmate.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+private ActivitySignInBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_sign_in);
+        binding.loginToSignupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
